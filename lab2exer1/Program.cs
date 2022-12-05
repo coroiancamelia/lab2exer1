00552b2b02332c780a79edf6c9d788cd1e2b54f4
coroiancamelia <coroiancamelia@outlook.com>
2022-12-05 10:25:10 +0200

    Add project files.

---
+++ lab2exer1/Program.cs
@@ -0,0 +1,32 @@
+﻿using System;
+
+namespace lab2exer1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+            /*Ex 1: Scrieti un program care va afisa suma cifrelor unui numar n, n fiind
+            citit de la tastatura.*/
+
+            Console.WriteLine("Introduceti un numar:");
+            int y = int.Parse(Console.ReadLine());
+            int z;
+            int suma = 0;
+
+            while (y > 0)
+
+            {
+                z = y % 10;
+                suma = suma + z;
+                y = y / 10;
+
+            }
+            Console.WriteLine("Suma cifrelor este:" + suma);
+
+
+
+        }
+    }
+}
